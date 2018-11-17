0cda40c083d8bca20e9370701d858d566b352239
hmaher2 <36460457+hmaher2@users.noreply.github.com>
2018-11-17 23:03:43 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a [10];
+    cout << "enter a" << endl;
+    cin>>a;
+    int x = 0;
+
+    for (int i = 0 ; a[i];i++ )
+
+    {  x++ ;}
+    cout << "x is " << x;
+
+
+    return 0;
+}
